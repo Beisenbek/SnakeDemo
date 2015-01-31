8d43fbd6d720376a4198193b758e99f4be47ad4a
beysenbek <beysenbek@gmail.com>
2015-02-01 02:07:28 +0400

    Samples from snake demo

---
+++ Snake/Items/Snake.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake.Items
+{
+    public class MySnake
+    {
+        public char sign;
+        List<Point> body = new List<Point>();
+    }
+}
---
+++ Snake/Program.cs
@@ -0,0 +1,48 @@
+﻿using Snake.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MySnake worm = new MySnake();
+
+            ConsoleKeyInfo keyInfo;
+
+            while (true)
+            {
+                keyInfo = Console.ReadKey();
+
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        Console.WriteLine("Up");
+                        break;
+                    case ConsoleKey.DownArrow:
+                        Console.WriteLine("Down");
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        Console.WriteLine("Left");
+                        break;
+                    case ConsoleKey.RightArrow:
+                        Console.WriteLine("Right");
+                        break;
+                    case ConsoleKey.Escape:
+                        return;
+
+                    default: break;
+                }
+
+               
+
+            }
+
+        }
+    }
+}
